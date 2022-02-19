106fa065f597bcef86e7cb2f8a922d602489bc1b
aliceFeldhaus <70300550+aliceFeldhaus@users.noreply.github.com>
2022-02-19 15:54:28 -0300

    Update Asteriscos.java

--- Desafios_Capgemini/src/Asteriscos.java
+++ Desafios_Capgemini/src/Asteriscos.java
@@ -5,9 +5,9 @@ public class Asteriscos {
 	public static void main(String[] arge) {
 
 		
-		int n = Integer.parseInt(JOptionPane.showInputDialog("Escreva um número: ")); //Solicita um número para criar a escada, e salva na variavel n
-		for (int i = 0; i < n; i++) { //Faz o laço de repetição pela quantidade de vezes informada pelo usuário (n)
-			for (int j = n - i; j > 1; j--) { //coloca os espaços em branco de forma decrescente
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Escreva um nÃºmero: ")); //Solicita um nÃºmero para criar a escada, e salva na variavel n
+		for (int i = 0; i < n; i++) { //Faz o laÃ§o de repeticao pela quantidade de vezes informada pelo usuario (n)
+			for (int j = n - i; j > 1; j--) { //coloca os espaÃ§os em branco de forma decrescente
 				System.out.print(" ");
 			}
 			for (int j = 0; j <= i; j++) { //Coloca os asteriscos de forma crescente
